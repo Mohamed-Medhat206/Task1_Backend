32fe06208c5867d0c0846c65a80d7402e3461af8
Mohamed-Medhat206 <https://github.com/Mohamed-Medhat206>
2026-04-20 14:44:58 +0200

    task2 'finish The 'VIP Status' Logic

--- core/models.py
+++ core/models.py
@@ -8,28 +8,32 @@ class baseModel(models.Model):
         abstract = True
 
 class Branch(baseModel):
-    name = models.CharField()
-    location = models.CharField()
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
     
     def __str__(self):
         return self.name
 
 class Member(baseModel):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
 
+    @property
+    def is_vip(self):
+        return self.balance > 1000
+
 class Trainer(baseModel):
-    name = models.CharField()
-    specialization = models.CharField()
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class GymClass(baseModel):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     start_date = models.DateField()
     trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
@@ -39,7 +43,7 @@ class GymClass(baseModel):
         return self.title
 
 class Equipment(baseModel):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     is_damaged = models.BooleanField(default=False)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
 
